71470c228a5b95500ff2ac9fcab77b234f4ceca8
Abhishek sunku <theroadrunnershow@gmail.com>
2015-05-03 23:45:50 -0700

    Added search to home page, need to call searchsource from home now. And cosmetics changes.

--- client/lib/startup.js
+++ client/lib/startup.js
@@ -1,3 +1,4 @@
 Meteor.startup(function () {
     	GoogleMaps.load({ v: '3', key: 'AIzaSyD_a9DwOndfi4_5pyOAKcn0xIT-FqCHT_I', libraries: 'geometry,places' });
+    	Meteor.Loader.loadCss('http://fonts.googleapis.com/css?family=Montserrat');
     });
